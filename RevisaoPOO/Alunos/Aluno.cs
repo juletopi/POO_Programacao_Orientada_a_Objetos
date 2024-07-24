b78ba533159f38d196d23c28f37731176a9a6b5b
Júlio Cézar <juliocezarpvh@hotmail.com>
2024-07-23 21:11:22 -0400

    Commit do conteúdo da aula ministrada em 17/07

---
+++ RevisaoPOO/Alunos/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RevisaoPOO.Alunos
+{
+    public class Aluno // <-- Classe "Aluno"
+    {
+        // Atributos da classe "Aluno"
+        public string nome; // <-- Nome do aluno
+        public DateTime dataNasc; // <-- Data de nascimento do aluno
+        public int idade; // <-- Idade do aluno
+
+        // Método construtor da classe "Aluno"
+        public Aluno(string nome, DateTime dataNasc) 
+        {
+            this.nome = nome; // <-- Parâmetro do atributo "nome"
+            this.dataNasc = dataNasc; // <-- Parâmetro do atributo "dataNasc"
+
+            // Calcula a idade do aluno com base no ano atual e no ano de nascimento do aluno
+            idade = DateTime.Now.Year - dataNasc.Year;
+
+            // Ajusta a idade caso a data de aniversário do aluno ainda não chegou no ano corrente
+            if (DateTime.Now.DayOfYear < dataNasc.DayOfYear)
+            {
+                idade--; // <-- Reduz 1 ano se o aluno ainda não fez aniversário este ano
+            }
+        }
+
+        // Método para exibir informações do aluno
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {nome}"); // <-- Exibe o nome do aluno
+            Console.WriteLine($"Data de Nascimento: {dataNasc.ToShortDateString()}"); // <-- Exibe a data de nascimento do aluno formatada como string
+            Console.WriteLine($"Idade: {idade}"); // <-- Exibe a idade do aluno
+        }
+    }
+}
---
+++ RevisaoPOO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using RevisaoPOO.Alunos;
+
+namespace RevisaoPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicita ao usuário o nome do aluno
+            Console.Write("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            // Solicita ao usuário a data de nascimento do aluno
+            Console.Write("Digite a data de nascimento do aluno (dd/MM/yyyy): ");
+            DateTime dataNasc;
+
+            // Tenta converter a entrada do usuário para DateTime até que uma data válida seja fornecida
+            while (!DateTime.TryParse(Console.ReadLine(), out dataNasc))
+            {
+                Console.Write("Data inválida. Digite a data de nascimento do aluno (dd/MM/yyyy): ");
+            }
+
+            // Criação de um objeto da classe "Aluno"
+            Aluno a1 = new Aluno(nome, dataNasc);
+
+            // Chama o método para exibir as informações do aluno
+            a1.ExibirInformacoes();
+
+            Console.ReadKey();
+        }
+    }
+}
